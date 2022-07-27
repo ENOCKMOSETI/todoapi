821bba5a9abef7043dcdb8f923b2845efc1ec54d
MOSETI <enock.g.moseti@gmail.com>
2022-07-28 00:39:37 +0300

    fix: production database connection

--- db/connection.js
+++ db/connection.js
@@ -12,10 +12,9 @@ if (process.env.NODE_ENV === 'production') {
             },
         }
     );
-} else {
-    const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-        host: process.env.DB_HOST,
-        dialect: 'postgres',
-    });
 }
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+});
 module.exports = sequelize;
