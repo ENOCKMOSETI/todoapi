61b02266eaec39e15efcf8bcfeef4aa17a9e5938
MOSETI <enock.g.moseti@gmail.com>
2022-07-28 00:37:33 +0300

    fix: production database connection

--- db/connection.js
+++ db/connection.js
@@ -2,9 +2,20 @@ const { Sequelize } = require('sequelize');
 
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-});
-
+if (process.env.NODE_ENV === 'production') {
+    module.exports = new Sequelize(process.env.DATABASE_URL, 
+        {
+            dialect: 'postgres',
+            protocol: 'postgres',
+            dialectOptions: {
+                ssl: true,
+            },
+        }
+    );
+} else {
+    const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+    });
+}
 module.exports = sequelize;
